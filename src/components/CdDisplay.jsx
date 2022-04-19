f9de6f6151b8d45388c685487f4d33b6ecfc1ff8
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-04-18 22:49:02 -0300

    Requisito 6 OK

---
+++ src/components/CdDisplay.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class CdDisplay extends React.Component {
+  render() {
+    const {
+      artistId,
+      artistName,
+      collectionId,
+      collectionName,
+      collectionPrice,
+      artworkUrl100,
+      releaseDate,
+      trackCount,
+    } = this.props;
+    return (
+      <div>
+        <p>{`ID do Artista: ${artistId}`}</p>
+        <p>{`Nome do Artista: ${artistName}`}</p>
+        <p>{`ID do CD: ${collectionId}`}</p>
+        <p>{`Nome do CD: ${collectionName}`}</p>
+        <p>{`Preço do CD: ${collectionPrice}`}</p>
+        <img src={ artworkUrl100 } alt={ collectionName } />
+        <p>{`Data do lançamento: ${releaseDate}`}</p>
+        <p>{`Número de faixas: ${trackCount}`}</p>
+        <Link
+          to={ `/album/${collectionId}` }
+          data-testid={ `link-to-album-${collectionId}` }
+        />
+      </div>
+    );
+  }
+}
+
+CdDisplay.propTypes = {
+  artistId: PropTypes.number.isRequired,
+  artistName: PropTypes.string.isRequired,
+  collectionId: PropTypes.number.isRequired,
+  collectionName: PropTypes.string.isRequired,
+  collectionPrice: PropTypes.number.isRequired,
+  artworkUrl100: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  trackCount: PropTypes.number.isRequired,
+};
+
+export default CdDisplay;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
+import CdDisplay from '../components/CdDisplay';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       artist: '',
+      loading: false,
+      returnArrayCd: {},
+      result: false,
+      searchInfo: '',
+      notValid: false,
     };
   }
 
@@ -17,30 +25,67 @@ class Search extends React.Component {
     });
   }
 
-  render() {
+  handleClick = async () => {
+    this.setState({ loading: true });
     const { artist } = this.state;
+    const arrayCds = await searchAlbumsAPI(artist);
+    if (arrayCds.length > 0) {
+      this.setState({
+        artist: '',
+        loading: false,
+        returnArrayCd: arrayCds,
+        result: true,
+        searchInfo: artist,
+        notValid: false,
+      });
+    } else {
+      this.setState({
+        artist: '',
+        loading: false,
+        notValid: true,
+      });
+    }
+  }
+
+  render() {
+    const { artist, loading, returnArrayCd, result, searchInfo, notValid } = this.state;
     const loginMinLength = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            placeholder="Which artist do you want to search?"
-            name="artist"
-            value={ artist }
-            onChange={ this.handleChange }
-          />
-
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ artist.length < loginMinLength }
-          >
-            Pesquisar
-          </button>
-        </form>
+        <div>
+          {!loading ? (
+            <form>
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                placeholder="Which artist do you want to search?"
+                name="artist"
+                value={ artist }
+                onChange={ this.handleChange }
+              />
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ artist.length < loginMinLength }
+                onClick={ this.handleClick }
+              >
+                Pesquisar
+              </button>
+            </form>
+          ) : (
+            <Loading />
+          ) }
+        </div>
+        {result && (
+          <section>
+            <p>{`Resultado de álbuns de: ${searchInfo}`}</p>
+            <div>
+              {returnArrayCd.map((cd) => <CdDisplay key={ cd.collectionId } { ...cd } />)}
+            </div>
+          </section>
+        )}
+        <div>{notValid && <p>Nenhum álbum foi encontrado</p>}</div>
       </div>
     );
   }
